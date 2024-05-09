e26f8cb19ba74bc75835073d8af71bfd48dfbb9a
akhilbcoder <akhilbcoder@gmail.com>
2024-05-09 07:40:02 +0530

    Conversions in js

---
+++ basics/conversions.js
@@ -0,0 +1,28 @@
+let a="1";
+let b = 2;
+// console.log( a+b);
+// console.log(1+2 +"2")
+// its beacuse toprimitive conversion is depend upon the first element present in log //
+// console.log(+true)=>1
+// console.log(true)=>true
+// console.log("1" +2 +2);
+let num1="";
+console.log(Boolean(num1));
+
+
+// console.log(1+2+"2")
+let gamecounter=100;
+// console.log(++gamecounter)
+// console.log(gamecounter++)
+
+/**********postfix************/
+let x =2;
+const y =x++;
+// in postfix the increment occur after assignining the value to the assign variable //
+
+/******prefix********/
+let c =++x;
+
+// in prefix first the increment occur then the value is assign to the assign variable //
+
+// console.table([x,y,c]);
